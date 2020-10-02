54c2a3238433b4035453f5c0d1057f3eb194bd4c
David <davi1360@stud.kea.dk>
2020-10-02 12:22:43 +0200

    Daos

---
+++ Models/Entities/Instrument.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicDating.Models.Entities 
+{
+    public class Instrument 
+    {
+        public int InstrumentId { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+
+        // Navigation properties
+        public ICollection<Agent> Agents { get; set; }
+    }
+}
